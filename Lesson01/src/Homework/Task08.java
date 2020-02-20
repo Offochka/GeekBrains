b51451666194aa3a5a855af12b7654887c75404c
Offochka <offochka007@gmail.com>
2020-02-20 15:46:54 +0200

    Changes homework

--- Lesson01/src/Homework/Task08.java
+++ Lesson01/src/Homework/Task08.java
@@ -3,13 +3,9 @@ package Homework;
 в консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.*/
 public class Task08 {
     public static void main (String[]args){
-        NewYear(2021);
+        System.out.println("Год является" + ((NewYear(2021)) ? ("високосным."): (" НЕ високосным.")));
     }
-    public static void NewYear(int year){
-        if (((year % 4 == 0) && !(year % 100 == 0)) || ( year % 400 ==0 )){
-            System.out.println("Год является високосным.");
-        }else {
-            System.out.println("Год НЕ является високосным.");
-        }
+    public static boolean NewYear(int year){
+        return  (((year % 4 == 0) && !(year % 100 == 0)) || ( year % 400 ==0 ));
     }
 }
