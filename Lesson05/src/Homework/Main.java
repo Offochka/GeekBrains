1111204f785c55feb3d933568903f83e2002cd70
Offochka <offochka007@gmail.com>
2020-03-10 01:33:05 +0200

    change Homework05

--- Lesson05/src/Homework/Main.java
+++ Lesson05/src/Homework/Main.java
@@ -5,14 +5,15 @@ package Homework;
  * Создать массив из 5 сотрудников
  * С помощью цикла вывести информацию только о сотрудниках старше 40 лет;*/
 
- public class Main {
+public class Main {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-        employees [0] = new Employee("Kate","Beris","Ivanovna",20,"Student","lasop@gron.ru",95631586,1500);
-        employees [1] = new Employee("Nikolas","Petrov","Sergeevich",47,"Cook","kidlo@gron.ru",45875687,50000);
-        employees [2] = new Employee("Alex","Toys","Petrovich",40,"Waiter","fdgsgh@gron.ru",23896745,40000);
-        employees [3] = new Employee("Sergei","Lomov","Stanislavovich",42,"Manager","rsgdtg@gron.ru",3685867,60000);
-        employees [4] = new Employee("Tom","Borisov","Anatolevich",25,"Painter","gdrgdrg@gron.ru",98736542,30000);
+        Employee[] employees = {
+                new Employee("Kate", "Beris", "Ivanovna", 20, "Student", "lasop@gron.ru", 95631586, 1500),
+                new Employee("Nikolas", "Petrov", "Sergeevich", 47, "Cook", "kidlo@gron.ru", 45875687, 50000),
+                new Employee("Alex", "Toys", "Petrovich", 40, "Waiter", "fdgsgh@gron.ru", 23896745, 40000),
+                new Employee("Sergei", "Lomov", "Stanislavovich", 42, "Manager", "rsgdtg@gron.ru", 3685867, 60000),
+                new Employee("Tom", "Borisov", "Anatolevich", 25, "Painter", "gdrgdrg@gron.ru", 98736542, 30000)
+        };
 
         for (Employee i : employees) {
             if (i.getAge() > 40) {
